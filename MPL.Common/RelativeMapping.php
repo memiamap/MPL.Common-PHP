ad065b3101fcd55c4de5fc6806d2d21e24e51402
memiamap <github@memia.biz>
2021-01-27 22:28:48 +0000

    Added additional relative mapping functions

--- MPL.Common/RelativeMapping.php
+++ MPL.Common/RelativeMapping.php
@@ -3,9 +3,28 @@ declare(strict_types=1);
 
 namespace MPL\Common
 {
-    use MPL\Common\ErrorHandling;
+  use MPL\Common\ErrorHandling;
+
   class RelativeMapping
   {
+    // Private functions
+    private static function mapRelativeFile(string $file): ?string {
+      $returnValue = null;
+      
+    	if (isset($_SERVER['APPL_PHYSICAL_PATH'])) {
+    		$returnValue = $_SERVER['APPL_PHYSICAL_PATH'] . $file;		
+    	} else if (isset($_SERVER['URL'])) {
+    		$levels = substr_count(str_replace(self::GetSiteRelativeUrlSegment(), '', $_SERVER['URL']), '/') - 1;
+    		$returnValue = $file;
+    		while ($levels-- > 0)
+    			$returnValue = '../' . $returnValue;				
+    	} else if (isset($_SERVER['DOCUMENT_ROOT'])) {
+    		$returnValue = $_SERVER['DOCUMENT_ROOT'] . $file;
+    	}
+    	
+    	return $returnValue;
+    }
+
     // Public functions
     public static function GetSiteRelativeUrlSegment(): string {
       $returnValue = '';
@@ -43,21 +62,27 @@ namespace MPL\Common
     	return $returnValue;
     }
 
+    public static function MapRelativeUrlWithTimestamp(string $path): string {
+      $returnValue = null;
+
+      // Get the physical path of the file and verify it exists
+    	$physicalPath = self::MapRelativePath($path);
+    	if (file_exists($physicalPath)) {
+    	  $fileTime = filemtime($physicalPath);
+      	$returnValue = self::MapRelativeUrl($path) . "?{$fileTime}";
+    	} else {
+    	  throw new \Exception("Cannot map physical path to '{$physicalPath}': File does not exist");
+    	}
+    	
+    	return $returnValue;
+    }
+
     public static function OutputRelativeUrl(string $path): void {
     	echo self::MapRelativeUrl($path);
     }
 
     public static function RelativeInclude(string $file, bool $includeOnce = true): void {
-    	if (isset($_SERVER['URL'])) {
-    		$levels = substr_count(str_replace(self::GetSiteRelativeUrlSegment(), '', $_SERVER['URL']), '/') - 1;
-    	} else {
-    		$levels = substr_count(str_replace(self::GetSiteRelativeUrlSegment(), '', $_SERVER['SCRIPT_FILENAME']), '/') - 1;
-    	}
-
-  		while ($levels-- > 0) {
-  			$file = '../' . $file;
-  		}
-
+      $file = self::mapRelativeFile($file);
       if ($includeOnce) {
     	  include_once $file;
     	} else {
@@ -65,16 +90,16 @@ namespace MPL\Common
     	}
     }
 
+    public static function RelativeIncludeToString(string $file): string {
+      $file = self::mapRelativeFile($file);
+      ob_start();
+      include $file;
+      $returnValue = ob_get_clean();
+  	  return $returnValue;
+    }
+
     public static function RelativeRequire(string $file, bool $requireOnce = true): void {
-    	if (isset($_SERVER['APPL_PHYSICAL_PATH'])) {
-    		$file = $_SERVER['APPL_PHYSICAL_PATH'] . $file;		
-    	} else if (isset($_SERVER['URL'])) {
-    		$levels = substr_count(str_replace(self::GetSiteRelativeUrlSegment(), '', $_SERVER['URL']), '/') - 1;
-    		while ($levels-- > 0)
-    			$file = '../' . $file;				
-    	} else if (isset($_SERVER['DOCUMENT_ROOT'])) {
-    		$file = $_SERVER['DOCUMENT_ROOT'] . $file;
-    	}
+      $file = self::mapRelativeFile($file);
       if ($requireOnce) {
     	  require_once $file;
     	} else {
