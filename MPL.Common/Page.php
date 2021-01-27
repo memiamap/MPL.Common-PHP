d885f560629766d9e95ac0068bb233261daea7bd
memiamap <github@memia.biz>
2021-01-27 22:28:18 +0000

    Added function to return canonical URL for current webpage

--- MPL.Common/Page.php
+++ MPL.Common/Page.php
@@ -6,6 +6,11 @@ namespace MPL\Common
   class Page
   {
     // Public functions
+    // Returns the canonical URL for the current page.
+    public static function GetCanonicalUrl(): string {
+	    return Website::$BaseLocation . str_replace(RelativeMapping::GetSiteRelativeUrlSegment(), '', strtok($_SERVER["REQUEST_URI"], '?'));
+    }
+
     // Returns the running page URL including sub-folders.
     public static function GetRunningPageUrl(): string {
       // Get the running file and strip the app path from it		
