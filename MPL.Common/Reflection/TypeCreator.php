391cc497b70153bd1067facfbc7afb3d832a4303
memiamap <github@memia.biz>
2021-02-13 20:25:35 +0000

    Added methods to try type creation for graceful failure

--- MPL.Common/Reflection/TypeCreator.php
+++ MPL.Common/Reflection/TypeCreator.php
@@ -14,8 +14,12 @@ namespace MPL\Common\Reflection
       $returnValue = null;
 
       try {
-    	  // Load the assembly from the filename
-    	  require_once $fileName;
+        if (file_exists($fileName)) {
+      	  // Load the assembly from the filename
+      	  require_once $fileName;
+      	} else {
+      	  throw new \Exception("The specified file '{$fileName}' does not exist");
+      	}
       } catch (\Throwable $t) {
 			  ErrorHandling::LogThrowable($t, 'Load assembly failed');
 			  throw new \Exception("Unable to load relative file '{$fileName}'");
@@ -60,6 +64,31 @@ namespace MPL\Common\Reflection
 			  throw new \Exception("The class '{$className}' does not exist");
       }
 
+      return $returnValue;
+    }
+
+    public static function TryCreateType(string $fileName, string $className, ?object &$output): bool {
+      $returnValue = false;
+      
+      try {
+        $output = self::CreateType($fileName, $className);
+        $returnValue = true;
+      } catch (\Throwable $t) {
+      }
+      
+      return $returnValue;
+    }
+
+    public static function TryCreateTypeRelative(string $fileName, string $className, ?object &$output): bool {
+      $returnValue = false;
+      
+      try {
+        $output = self::CreateTypeRelative($fileName, $className);
+        $returnValue = true;
+      } catch (\Throwable $t) {
+        var_dump('XXX');
+      }
+      
       return $returnValue;
     }
   }
